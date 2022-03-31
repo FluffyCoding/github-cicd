b73d0a3da9c08506949b2d8ca9db480a2abcab42
ShahzadItech <shahzad.itech@yahoo.com>
2022-03-31 16:14:31 +0400

    GITHUB ACTION CICD 1..

---
+++ src/main/java/com/unity/cicd/CicdApplication.java
@@ -0,0 +1,22 @@
+package com.unity.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApplication.class, args);
+	}
+
+
+	@GetMapping(path = "/hello")
+	public String message(){
+		return "HELLO GITHUB CICD";
+	}
+
+}
